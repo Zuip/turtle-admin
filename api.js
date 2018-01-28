0e5f71e5cc045982eb030530bd8bfcb5bb3798c4
Zuip <robert.nurminen@iki.fi>
2018-01-28 17:22:10 +0100

    Moved the main Nodejs server file to project root

--- api.js
+++ api.js
@@ -3,11 +3,11 @@ let app = express();
 let path = require("path");
 
 // Allow reading files from public folder
-app.use('/', express.static(path.join(__dirname, '.')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 // The frontend
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/main.html'));
+  res.sendFile(path.join(__dirname + '/public/main.html'));
 });
 
 // API
